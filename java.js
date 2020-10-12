bd59fa2fde89b28276afde5a8a67915b7119da2e
PoudelSandeeep <72459058+PoudelSandeeep@users.noreply.github.com>
2020-10-12 13:11:46 +0545

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+var a = confirm("This site contains sensitive information. Are you sure you want to continue?")
+if (a) {
+}
+else{
+window.location="https://codepen.io/akm2/full/rHIsa?__cf_chl_jschl_tk__=e194668e1bb2fa94e9553b37759f1aedf3d68d34-1602345923-0-AbUZzKzmVwfju-EwduEDKnslz1sqP8ujeHhC2skDvv77crHtRJ_uP0Ej5QZVvU4qBoV9v5rmwhiZuP9xJJlFm7V4kSNRvFaNWYk3gRefnF6d87Kdlup0G7N1rFhHaWMT6TobLDl495FkzHgCJ3TxRvd6TMjwWH_r3HdcvGzIdKENW4I34IRsN29CxFnm7vei7-4u5OVJlIw5zT5tp5TBzhgxIpgW6xjp8yKVFm_q65HDdkR9Jd_mCINU0DSzNYoX6cOSSPvsgMNlktZ6Re5juj7aS0oB8myMuU_4VvK_0jsX-Idkn7zN8i1kT5xE0hC9Sdf8_l55yd8PDtNdH-PddLtufxD-p3iKNOi8y27lxfJ0"
+}
+function button(){
+var b= confirm("Leave this site?")
+if (b) {
+window.location="https://github.com/TechnicalAli433/Dark-FB-Gold"
+}
+else{
+alert("Thanks for sticking around!")
+}
+}
