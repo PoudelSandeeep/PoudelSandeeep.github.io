beeee83190d767078390fccb5edf0c310358b9ea
PoudelSandeeep <72459058+PoudelSandeeep@users.noreply.github.com>
2020-10-18 20:32:19 +0545

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+var a = confirm("This site contains sensitive information. Are you sure you want to continue?")
+if (a) {
+}
+else{
+window.location="https://www.google.com/"
+}
+
+function button(){
+var b= confirm("Leave this site?")
+if (b) {
+    window.location="https://github.com/TechnicalAli433/Dark-FB-Gold"
+}
+else{
+alert("Thanks for sticking around!")
+}
+}
